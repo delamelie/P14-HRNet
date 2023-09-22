d8a8b8cc452a75d6ea32300773ec3785dae3dc94
Amélie de Lima <amelie.delima@hotmail.fr>
2023-09-22 14:35:52 +0200

    reset custom loader

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes.js";
 import "./index.css";
 
-//const Loader = lazy(() => import("./components/table/Loader"));
+const Loader = lazy(() => import("./components/table/Loader"));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Suspense fallback={<span>Loading...</span>}>
+    <Suspense fallback={<Loader />}>
       <RouterProvider router={router} />
     </Suspense>
   </React.StrictMode>
